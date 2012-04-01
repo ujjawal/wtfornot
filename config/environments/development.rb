4893542b33b5c6a0694810e56c01f613073fe9c4
ujjawal <ujjawal@thoughtnirvana.com>
2012-04-02 02:26:27 +0530

    dev config action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,8 @@ Wtfornot::Application.configure do
 
   # Default URL option for devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {:address => "localhost", :port => 1025}
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
