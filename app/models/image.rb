2dea80bb3580fbbce6c834f14317f12e61c87d1d
ujjawal <ujjawal@thoughtnirvana.com>
2012-04-04 04:01:26 +0530

    validations

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,7 +1,18 @@
+class ContentTypeValidator < ActiveModel::Validator
+  def validate(record)
+    if !record.file_content_type.blank? and !['image/jpeg', 'image/gif','image/png'].include?(record.file_content_type)
+      record.errors[:base] << "Image '#{record.file_file_name}' is not a valid image type"
+    end
+  end 
+end
+
 class Image < ActiveRecord::Base
 
   paginates_per 4
+
   validates :file_file_name, :file_content_type, :file_file_size, :presence => true
+  
+  validates_with ContentTypeValidator
 
   belongs_to :user
 
@@ -25,6 +36,7 @@ class Image < ActiveRecord::Base
     :s3_credentials => S3_CREDENTIALS,
     :path => ":attachment/:id/:style.:extension",
     :convert_options => { :all => "-auto-orient" }
-  validates_attachment_content_type :file, :content_type => ['image/jpeg', 'image/png','image/gif']
-  validates_attachment_size :file, :less_than => 1.megabytes
+  validates_attachment_size :file, :less_than => 1.megabytes, 
+    :message => "Cannot digest files bigger than 1MB. Try again."
+  
 end
