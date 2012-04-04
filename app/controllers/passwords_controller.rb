3741c257fdc8f33f67e749403f4ea4cd9f359680
ujjawal <ujjawal@thoughtnirvana.com>
2012-04-04 14:49:53 +0530

    password recovery: not found for invalid username

--- app/controllers/passwords_controller.rb
+++ app/controllers/passwords_controller.rb
@@ -8,15 +8,18 @@ class PasswordsController < Devise::PasswordsController
   # POST /resource/password
   def create
     user = User.where(:username => params[resource_name][:username]).first
-    params[resource_name][:email] = user.email
-    params[resource_name].delete("username")
-    self.resource = resource_class.send_reset_password_instructions(params[resource_name])
+    if user
+      params[resource_name][:email] = user.email
+      params[resource_name].delete("username")
+      self.resource = resource_class.send_reset_password_instructions(params[resource_name])
 
-    if successfully_sent?(resource)
-      respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
+      if successfully_sent?(resource)
+        respond_with({}, :location => after_sending_reset_password_instructions_path_for(resource_name))
+      else
+        respond_with(resource)
+      end
+    else  
+      raise ActionController::RoutingError.new('Not Found')
     end
   end
-
 end
